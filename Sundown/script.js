693ec9bf773b5256abc3857457a4d810902f9448
Ratan <ratanrathod870@gmail.com>
2024-05-27 12:25:13 +0530

    page 4 code / some debugging

--- Sundown/script.js
+++ Sundown/script.js
@@ -4,21 +4,35 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-// page 3
-var elemC = document.querySelector("#elem-container")
-var fixed = document.querySelector("#fixed-image")
-elemC.addEventListener("mouseenter", function () {
-    fixed.style.display = "block"
-})
-
-elemC.addEventListener("mouseleave", function () {
-    fixed.style.display = "none"
-})
-
-var elems = document.querySelectorAll(".elem")
-elems.forEach(function (e) {
-    e.addEventListener("mouseenter", function () {
-        var image = e.getAttribute("data-image")
-        fixed.style.backgroundImage = `url(${image})`
+function page4Animation() {
+    // page 4 
+    var elemC = document.querySelector("#elem-container")
+    var fixed = document.querySelector("#fixed-image")
+    elemC.addEventListener("mouseenter", function () {
+        fixed.style.display = "block"
     })
-})
+
+    elemC.addEventListener("mouseleave", function () {
+        fixed.style.display = "none"
+    })
+
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
+            var image = e.getAttribute("data-image")
+            fixed.style.backgroundImage = `url(${image})`
+        })
+    })
+}
+
+// swiper js 
+function swiperAnimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: false,
+        spaceBetween: 50,
+    });
+}
+
+swiperAnimation()
+page4Animation()
