378828eeae136d47fe837e7962e6552a1319f4ad
Ratan <ratanrathod870@gmail.com>
2024-05-27 15:14:05 +0530

    responsive code and hamburgur menu code

--- Sundown/script.js
+++ Sundown/script.js
@@ -36,3 +36,24 @@ function swiperAnimation() {
 
 swiperAnimation()
 page4Animation()
+
+function menuAnimation() {
+
+    var menu = document.querySelector("nav h3")
+    var full = document.querySelector("#full-scr")
+    var navimg = document.querySelector("nav img")
+    var flag = 0
+    menu.addEventListener("click", function () {
+        if (flag == 0) {
+            full.style.top = 0
+            navimg.style.opacity = 0
+            flag = 1
+        } else {
+            full.style.top = "-100%"
+            navimg.style.opacity = 1
+            flag = 0
+        }
+    })
+}
+
+menuAnimation()
