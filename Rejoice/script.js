c3fb55c32dad7a45078d26558f7f2a3f1c84f9ef
Ratan <ratanrathod870@gmail.com>
2024-07-21 01:42:25 +0530

    added gsap

--- Rejoice/script.js
+++ Rejoice/script.js
@@ -1,7 +1,25 @@
-var page1Content = document.querySelector(".page1-content")
-var cursor = document.querySelector(".cursor")
+function cursorEffect() {
+    var page1Content = document.querySelector(".page1-content")
+    var cursor = document.querySelector(".cursor")
 
-page1Content.addEventListener("mousemove", function(dets){
-    cursor.style.left = dets.x+"px"
-    cursor.style.top = dets.y+"px"
-})
+    page1Content.addEventListener("mousemove", function (dets) {
+        gsap.to(".cursor", {
+            x: dets.x,
+            y: dets.y
+        })
+    })
+
+    page1Content.addEventListener("mouseenter", function () {
+        gsap.to(cursor, {
+            scale: 1
+        })
+    })
+
+    page1Content.addEventListener("mouseleave", function () {
+        gsap.to(cursor, {
+            scale: 0
+        })
+    })
+}
+
+cursorEffect()
