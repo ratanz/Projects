03dc0621439a825b35b20d5065920a0a73b5a998
Ratan <ratanrathod870@gmail.com>
2024-07-21 21:37:48 +0530

    page 2 and 3 almost work done

--- Rejoice/script.js
+++ Rejoice/script.js
@@ -1,3 +1,4 @@
+
 function cursorEffect() {
     var page1Content = document.querySelector(".page1-content")
     var cursor = document.querySelector(".cursor")
@@ -23,3 +24,21 @@ function cursorEffect() {
 }
 
 cursorEffect()
+
+function page2Animation() {
+    gsap.from(".page2 .text2 span", {
+        y: 10,
+        stagger : 0.25,
+        duration: 1,
+        scrollTrigger : {
+            trigger : ".page2",
+            scroller : ".main",
+            start : "top 47%",
+            end : "top 37%",
+            scrub : 2
+        }
+    });
+}
+
+page2Animation()
+
